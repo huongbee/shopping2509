affcbd02c7b1c4f8e069173b373dc0f1abd1489d
Huong <admin@Apples-MacBook-Pro.local>
2018-12-01 21:20:48 +0700

    special product

--- view/index.view.php
+++ view/index.view.php
@@ -57,17 +57,22 @@
                     <div class="slider-items-products">
                       <div id="featured-slider" class="product-flexslider hidden-buttons">
                         <div class="slider-items slider-width-col4">
+                          
                           <?php foreach($data['specialProduct'] as $product):?>
                           <div class="product-item">
                             <div class="item-inner">
                               <div class="product-thumbnail">
+                                <?php if($product->promotion_price!=0):?>
                                 <div class="icon-sale-label sale-left">Sale</div>
+                                <?php endif?>
+                                <?php if($product->new==1):?>
                                 <div class="icon-new-label new-right">New</div>
+                                <?php endif?>
                                 <div class="pr-img-area">
-                                  <a title="Ipsums Dolors Untra" href="single_product.html">
+                                  <a title="<?=$product->name?>" href="single_product.html">
                                     <figure>
-                                      <img class="first-img" src="public/images/products/img01.jpg" alt="html template">
-                                      <img class="hover-img" src="public/images/products/img01.jpg" alt="html template">
+                                      <img class="first-img" src="public/images/products-images/<?=$product->image?>" alt="html template">
+                                      <img class="hover-img" src="public/images/products-images/<?=$product->image?>" alt="html template">
                                     </figure>
                                   </a>
                                   <button type="button" class="add-to-cart-mt">
@@ -79,19 +84,23 @@
                               <div class="item-info">
                                 <div class="info-inner">
                                   <div class="item-title">
-                                    <a title="Ipsums Dolors Untra" href="single_product.html">Ipsums Dolors Untra </a>
+                                    <a title="<?=$product->name?>" href="single_product.html"><?=$product->name?></a>
                                   </div>
                                   <div class="item-content">
                                     <div class="item-price">
                                       <div class="price-box">
+                                        <?php if($product->promotion_price!=0):?>
                                         <p class="special-price">
-                                          <span class="price-label">Special Price</span>
-                                          <span class="price"> $456.00 </span>
+                                          <span class="price"> <?=number_format($product->promotion_price)?> </span>
                                         </p>
                                         <p class="old-price">
-                                          <span class="price-label">Regular Price:</span>
-                                          <span class="price"> $567.00 </span>
+                                          <span class="price"> <?=number_format($product->price)?> </span>
                                         </p>
+                                        <?php else:?>
+                                        <p class="special-price">
+                                          <span class="price"> <?=number_format($product->price)?> </span>
+                                        </p>
+                                        <?php endif?>
                                       </div>
                                     </div>
                                   </div>
