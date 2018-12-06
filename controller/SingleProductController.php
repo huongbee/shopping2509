6be223525fd341163c3c82396e48529c54d4b193
Huong <admin@Apples-MacBook-Pro.local>
2018-12-06 19:58:20 +0700

    url single product

--- controller/SingleProductController.php
+++ controller/SingleProductController.php
@@ -3,6 +3,9 @@ include_once 'BaseController.php';
 
 class SingleProductController extends BaseController{
     function getProductPage(){
+        $url = $_GET['url'];
+        $id = $_GET['id'];
+        print_r($id);die;
         return $this->loadView('single-product');
     }
 }
